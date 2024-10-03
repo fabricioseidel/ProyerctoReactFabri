e987c7873755fbd1cbde26f9e4ad5e240d5e050f
fabricioseidel <seidelfabriciove@gmail.com>
2024-10-03 11:32:52 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+import Register from "./components/Register";
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Navbar />
+        <Home />
+        <Register />
+        <Login />
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default App;
